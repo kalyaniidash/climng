b03f2d8a190e2bee5928cac4c63c98e9206e40a7
kalyanidash <kalyanii.dash@gmail.com>
2024-01-27 07:28:55 +0000

    connect to database initiate

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const connectDB =async()=>{
+try{
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.DB_URI, { 
+        useNewUrlParser:true,
+        }).then(()=>{
+            console.log("successfully connect to mongoDB")
+        })
+}
+catch(err){
+    console.error(err.message)   
+    process.exit(1);
+}
+};
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express=require("express");
 //Initializing
+const connectDB = require("./config/db");
+const cors = require("cors");
+//Connect to mongodb
+connectDB();
+//initiating the app
 const app=express();
+
+//Enabling CORSs here for Safety OF access of info from other domains
+app.use(cors());
+//Handle JSON parsing 
+app.use(express.json())
+
 const port= process.env.PORT || 5000;
 app.listen(port,()=>
     console.log(`Server is Running on port ${port}`)
-
 );
