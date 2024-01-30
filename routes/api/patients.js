299ab19c49d7fd9252fd28f1d03fddd716442bb9
kalyanidash <kalyanii.dash@gmail.com>
2024-01-30 09:43:23 +0000

    add all the routes

--- routes/api/patients.js
+++ routes/api/patients.js
@@ -17,6 +17,63 @@ const router = express.Router();
   res.json({ ok:"all patients are there"})
  });
 
-
  
+// @route GET api/books/:id
+// @description Get single book by id
+// @access Public
+//READ ACCESS
+router.get('/:id', (req, res) => {
+   // Patient.findById(req.params.id)
+   // .then((patients) => res.json(patient))
+   // .catch((err) => res.status(404).json({ nopatientfound: 'No patient found' }));
+   res.json({
+       ok:"GET the patients by id"})
+});
+
+// @route POST api/patients
+// @description add/save patient
+// @access Public
+//CREATE ACCESS
+router.post('/', (req, res) => {
+//    Patient.create(req.body)
+//      .then((patient) => res.json({ 
+//       msg: 'patient added successfully' }))
+//      .catch((err) => res.status(400).json({ 
+//       error: 'Unable to add this patient' }));
+//  });
+ res.json({
+   ok:"patients created successfully"})
+});
+
+// @route GET api/patients/:id
+// @description Update patient
+// @access Public
+//READ,UPDATE SEARCH operation
+router.put('/:id', (req, res) => {
+//    Patient.findByIdAndUpdate(req.params.id, req.body)
+//      .then((patient) => res.json({
+//        msg: 'Updated successfully' }))
+//      .catch((err) =>
+//        res.status(400).json({ 
+//          error: 'Unable to update the Database' })
+//      );
+//  });
+ res.json({
+   ok:"update the patients by id  successfully"})
+});
+
+
+// @route GET api/patients/:id
+// @description find a patient by id and deleted
+// @access Public
+router.delete('/:id', (req, res) => {
+//    Patient.findByIdAndRemove(req.params.id, req.body)
+//      .then((patient) => res.json({ mgs: 'Patient entry deleted successfully' }))
+//      .catch((err) => res.status(404).json({ error: 'couldnt deletethis patient' }));
+//  });
+ 
+ res.json({
+   ok:"this patient deleted "})
+});
+
  module.exports = router;
