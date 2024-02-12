a87ffdf9cb823ed4136ec7085ec872fbb0ca1ed8
kalyanidash <kalyanii.dash@gmail.com>
2024-02-12 07:40:47 +0000

    added react toaster and add mongodb

--- client/src/components/CreatePatient.js
+++ client/src/components/CreatePatient.js
@@ -1,63 +1,139 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 import { useNavigate } from 'react-router-dom';
+import ShowPatientList from './ShowPatientList';
 import Navbar from './Navbar';
 import Footer from './Footer';
-
-const CreatePatient = (props) => { 
-  // Define the state with useState hook
  
+
+const CreatePatient = (props) => {
+  // Define the state with useState hook
+  const navigate = useNavigate();
+  const [patient, setPatient] = useState({
+    Patient_id: '',
+    Name: '',
+    Address: '',
+    Phone_no: '',
+    Gender: '',
+    Age: '',
+  });
+  const [showToast, setShowToast] = useState(false);
+
+  const onChange = (e) => {
+    setPatient({ ...patient, [e.target.name]: e.target.value });
+  };
   const onSubmit = (e) => {
-    //prevent the page for refreshing
+   // prevent the page from refreshing
     e.preventDefault();
-    //api calls with the help of axios
-    // axios.post("api/patients",patient).then((res)=>{
-    //    setPatient({
-    //     patient_id:"",
-    //     name:"",
-    //     address:"",
-    //     phone_no:"",
-    //     gender:"",
-    //     age:"",
-    //     join_date:"",
-    //    });
-    // })
-  }
+    //API calls with the help of axios
+    axios
+      .post('/api/patients', patient)
+      .then((res) => {
+        setPatient({
+          patient_id: '',
+          name: '',
+          address: '',
+          phone_no: '',
+          gender: '',
+          age: '',
+          join_date: '',
+        });
+      })
+      
+    }
+
   return (
     <div className='CreatePatient'>
-      < Navbar />
+      <Navbar/>
       <div className='container'>
         <div className='row'>
         <div className='col-md-8 m-auto'>
             <br />
-            <Link to='/all-patient' className='btn btn-outline-warning float-left'>
-              Show Patient List
+            <Link to='/all-patient ' className='btn btn-outline-warning float-left'>
+              show Patient List
             </Link>
           </div>
           <div className='col-md-8 m-auto'>
             <h1 className='display-4 text-center'>Add Patient</h1>
-            <p className='lead text-center'>Create new patient</p>
+            <p className='lead text-center'>Create new Patient</p>
+
             <form noValidate onSubmit={onSubmit}>
               <div className='form-group'>
                 <input
                   type='number'
-                  placeholder='enter your id'
+                  placeholder='Enter Your ID'
                   name='patient_id'
                   className='form-control'
-                  // value={Patient.title}
-                  // onChange={onChange}
+                  value={patient.id}
+                  onChange={onChange}
                 />
-                </div>
-                </form>
-          </div>
+                
+            </div>
+            <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Name'
+                  name='patient_name'
+                  className='form-control'
+                  value={patient.name}
+                  onChange={onChange}
+                />
+              </div>
 
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Address'
+                  name='patient_address'
+                  className='form-control'
+                  value={patient.address}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='Enter Your Phone.no'
+                  name='patient_phnno.'
+                  className='form-control'
+                  value={patient.phnno}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Enter Your Gender'
+                  name='patient_gender'
+                  className='form-control'
+                  value={patient.gender}
+                  onChange={onChange}
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='number'
+                  placeholder='Enter Your Age'
+                  name='patient_age'
+                  className='form-control'
+                  value={patient.age}
+                  onChange={onChange}
+                />
+              </div>
+
+
+
+            </form>
+            
+              </div>
         </div>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
 
-export default CreatePatient;
+export default CreatePatient;
